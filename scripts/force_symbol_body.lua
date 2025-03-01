8c64a8843da18885ffb2981f50e774c7187e73d0
Breathe <704340916@qq.com>
2025-03-01 22:19:00 +0800

    1.6.2：增加更多模组物品装备。

--- scripts/force_symbol_body.lua
+++ scripts/force_symbol_body.lua
@@ -1,5 +1,6 @@
 -- 部分物品需要强制到身体栏
 return {
     -- theemperorsmantle = "theemperorsmantle", -- 棱镜-皇帝的披风 不进行适配 默认装备在背包栏 因为它作用的是背包栏物品
+    muu_cane = "muu_cane",                --MUU千年之旅-雪橇犬的拐杖糖
     siving_suit_gold = "siving_suit_gold" -- 棱镜-子圭釜
 }
--- scripts/symbol_belly.lua
+++ scripts/symbol_belly.lua
@@ -1,19 +1,19 @@
 -- 定义服装栏物品
 return {
     -- 服装栏 原版物品
-    raincoat          = "raincoat",         -- 雨衣
-    onemanband        = "onemanband",       -- 独奏乐器
-    sweatervest       = "sweatervest",      -- 犬牙背心
-    beargervest       = "beargervest",      -- 熊皮大衣
-    balloonvest       = "balloonvest",      -- 充气背心
-    hawaiianshirt     = "hawaiianshirt",    -- 花衬衫
-    armorslurper      = "armorslurper",     -- 饥饿腰带
-    reflectivevest    = "reflectivevest",   -- 清凉夏装
-    trunkvest_summer  = "trunkvest_summer", -- 透气背心
-    trunkvest_winter  = "trunkvest_winter", -- 松软背心
-    carnival_vest_a   = "carnival_vest_a",  -- 叽叽喳喳围巾
-    carnival_vest_b   = "carnival_vest_b",  -- 叽叽喳喳斗篷
-    carnival_vest_c   = "carnival_vest_c",  -- 叽叽喳喳小披肩
+    raincoat           = "raincoat",        -- 雨衣
+    onemanband         = "onemanband",      -- 独奏乐器
+    sweatervest        = "sweatervest",     -- 犬牙背心
+    beargervest        = "beargervest",     -- 熊皮大衣
+    balloonvest        = "balloonvest",     -- 充气背心
+    hawaiianshirt      = "hawaiianshirt",   -- 花衬衫
+    armorslurper       = "armorslurper",    -- 饥饿腰带
+    reflectivevest     = "reflectivevest",  -- 清凉夏装
+    trunkvest_summer   = "trunkvest_summer", -- 透气背心
+    trunkvest_winter   = "trunkvest_winter", -- 松软背心
+    carnival_vest_a    = "carnival_vest_a", -- 叽叽喳喳围巾
+    carnival_vest_b    = "carnival_vest_b", -- 叽叽喳喳斗篷
+    carnival_vest_c    = "carnival_vest_c", -- 叽叽喳喳小披肩
 
     -- costume_fool_body = "costume_fool_body", -- 小丑服
     -- costume_doll_body = "costume_doll_body", -- 玩偶服
@@ -24,32 +24,33 @@ return {
     -- costume_blacksmith_body = "costume_blacksmith_body", -- 小丑服
 
     -- 服装栏 模组物品
-    down_filled_coat  = "down_filled_coat",          -- 能力勋章
-    cassock           = "cassock",                   -- 神话书说-袈裟
-    kam_lan_cassock   = "kam_lan_cassock",           -- 神话书说-锦斓袈裟
-    madameweb_armor   = "madameweb_armor",           -- 神话书说
-    sachet            = "sachet",                    -- 棱镜-香包
-    veneto_yifu       = "veneto_yifu",               -- 战舰少女维内托-小时候的衣服
-    dress_sea         = "dress_sea",                 -- 希儿-幻海梦蝶
-    seele_swimsuit    = "seele_swimsuit",            -- 希儿-夏雪铃兰
-    lg_fufeng         = "lg_fufeng",                 -- 海洋传说-雨花·扶风
-    krm_uniform       = "krm_uniform",               -- 狂三-校服
-    to_school         = "to_school",                 -- 鸢一折纸-校服
-    to_angel          = "to_angel",                  -- 鸢一折纸-绝灭天使
-    to_satan          = "to_satan",                  -- 鸢一折纸-救世魔王
-    woolen_sweater    = "woolen_sweater",            -- 山海秘藏-羊毛衫
-    ccs_skirt1        = "ccs_skirt1",                -- 魔法小樱-衣服
-    armor_lifejacket  = "armor_lifejacket",          -- 岛屿冒险-救生衣
-    tarsuit           = "tarsuit",                   -- 岛屿冒险-焦油套装
-    armor_windbreaker = "armor_windbreaker",         -- 岛屿冒险-防风衣
-    blubbersuit       = "blubbersuit",               -- 岛屿冒险-鲸脂套装
-    armor_snakeskin   = "armor_snakeskin",           -- 岛屿冒险-蛇皮夹克
-    myxl_dreambook    = "myxl_dreambook",            -- 璇儿-遗梦芸典
-    m_scarf           = "m_scarf ",                  -- M.louis-黑围巾
-    third_eye         = "third_eye ",                -- 古明地恋-第三只眼
-    third_eye2        = "third_eye2 ",               -- 古明地恋-增幅第三只眼
-    satori_eye        = "satori_eye ",               -- 古明地觉-第三只眼
-    satori_eye2       = "satori_eye2 ",              -- 古明地觉-增幅第三只眼
-    armor_carrotlure  = "armor_carrotlure  ",        -- 胡萝卜
-    zhifu             = "zhifu"                      -- 战舰少女补给包-秋冬制服
+    down_filled_coat   = "down_filled_coat",   -- 能力勋章
+    cassock            = "cassock",            -- 神话书说-袈裟
+    kam_lan_cassock    = "kam_lan_cassock",    -- 神话书说-锦斓袈裟
+    madameweb_armor    = "madameweb_armor",    -- 神话书说
+    sachet             = "sachet",             -- 棱镜-香包
+    veneto_yifu        = "veneto_yifu",        -- 战舰少女维内托-小时候的衣服
+    dress_sea          = "dress_sea",          -- 希儿-幻海梦蝶
+    seele_swimsuit     = "seele_swimsuit",     -- 希儿-夏雪铃兰
+    lg_fufeng          = "lg_fufeng",          -- 海洋传说-雨花·扶风
+    krm_uniform        = "krm_uniform",        -- 狂三-校服
+    to_school          = "to_school",          -- 鸢一折纸-校服
+    to_angel           = "to_angel",           -- 鸢一折纸-绝灭天使
+    to_satan           = "to_satan",           -- 鸢一折纸-救世魔王
+    woolen_sweater     = "woolen_sweater",     -- 山海秘藏-羊毛衫
+    ccs_skirt1         = "ccs_skirt1",         -- 魔法小樱-衣服
+    armor_lifejacket   = "armor_lifejacket",   -- 岛屿冒险-救生衣
+    tarsuit            = "tarsuit",            -- 岛屿冒险-焦油套装
+    armor_windbreaker  = "armor_windbreaker",  -- 岛屿冒险-防风衣
+    blubbersuit        = "blubbersuit",        -- 岛屿冒险-鲸脂套装
+    armor_snakeskin    = "armor_snakeskin",    -- 岛屿冒险-蛇皮夹克
+    myxl_dreambook     = "myxl_dreambook",     -- 璇儿-遗梦芸典
+    m_scarf            = "m_scarf ",           -- M.louis-黑围巾
+    third_eye          = "third_eye ",         -- 古明地恋-第三只眼
+    third_eye2         = "third_eye2 ",        -- 古明地恋-增幅第三只眼
+    satori_eye         = "satori_eye ",        -- 古明地觉-第三只眼
+    satori_eye2        = "satori_eye2 ",       -- 古明地觉-增幅第三只眼
+    armor_carrotlure   = "armor_carrotlure  ", -- 胡萝卜
+    fsm_yunmie_outfits = "fsm_yunmie_outfits", --山海秘藏-云咩套
+    zhifu              = "zhifu"               -- 战舰少女补给包-秋冬制服
 }
